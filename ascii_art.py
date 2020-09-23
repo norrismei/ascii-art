aad7086b3291086fd3043c05713e8b4db928b641
Norris Mei <norrismei3@gmail.com>
2020-09-22 21:44:38 -0700

    Can create canvas and add fixed rect w/ chosen char

--- ascii_art.py
+++ ascii_art.py
@@ -1,5 +1,33 @@
-import tkinter
+from tkinter import *
 
-root = tkinter.Tk()
+def create_canvas():
+    canvas = Canvas(master, width=600, height=300)
+    canvas.pack()
 
-root.mainloop()
+    return canvas
+
+def create_rect(char):
+    rect = ""
+
+    for y in range(3):
+        for x in range(5):
+            rect += char
+        rect += "\n"
+
+    return rect
+
+def draw_rectangle(rect):
+    canvas.create_text(50, 25, text=rect)
+
+master = Tk()
+
+canvas = create_canvas()
+
+draw = input("Would you like to draw a rectangle? y/n ")
+
+if draw == "y":
+    char = input("Enter a character: ")
+    rect = create_rect(char)
+    draw_rectangle(rect)
+
+mainloop()
