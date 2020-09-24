c29290af998e109130d2659d073717c6504ea05c
Norris Mei <norrismei3@gmail.com>
2020-09-23 19:27:26 -0700

    Refactored add shape function to utilize rectangles dict

--- ascii_art.py
+++ ascii_art.py
@@ -46,32 +46,45 @@ def get_fill_instructions(start_x, start_y, end_x, end_y):
     [False, False, False, False, False, False, False, False, False, False]]
 
     for y in range(start_y, end_y + 1):
-        print(f"y: {y}")
         for x in range(start_x, end_x + 1):
-            print(f"x: {x}")
             fill_matrix[y][x] = True
 
     return fill_matrix
 
 
-def update_canvas(canvas, fill_matrix, fill_char):
+def update_rect_dict(rectangles, fill_matrix, fill_char):
+    """Update dictionary with rectangle drawn"""
+
+    id = len(rectangles) + 1
+
+    rectangles[id] = [fill_char, fill_matrix]
+
+    return rectangles
+
+
+def update_canvas(canvas, rectangles):
     """Using fill_matrix instructions, update canvas with fill_char"""
 
-    for matrix_row, canvas_row in zip(fill_matrix, canvas):
-        for i in range(10):
-            if matrix_row[i] == True:
-                canvas_row[i] = fill_char
+    for rectangle in rectangles:
+        fill_char = rectangles[rectangle][0]
+        fill_matrix = rectangles[rectangle][1]
+
+        for matrix_row, canvas_row in zip(fill_matrix, canvas):
+            for i in range(10):
+                if matrix_row[i] == True:
+                    canvas_row[i] = fill_char
 
     return canvas
 
 
-def add_shape(canvas):
+def add_shape(canvas, rectangles):
     """Add a shape. For now, assume rectangle is only shape"""
 
     start_x, start_y, end_x, end_y, fill_char = get_attributes()
     fill_matrix = get_fill_instructions(start_x, start_y, end_x, end_y)
-    updated_canvas = update_canvas(canvas, fill_matrix, fill_char)
-    print_canvas(updated_canvas)
+    rectangles = update_rect_dict(rectangles, fill_matrix, fill_char)
+    canvas = update_canvas(canvas, rectangles)
+    print_canvas(canvas)
 
 
 def print_canvas(canvas):
@@ -116,7 +129,7 @@ def main():
         action = select_action()
 
         if action == "1":
-            add_shape(canvas)
+            add_shape(canvas, rectangles)
 
 
 if __name__ == '__main__':
