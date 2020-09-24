0fc4d99fabc56a3939b1f992c638c6e166c42332
Norris Mei <norrismei3@gmail.com>
2020-09-23 22:09:33 -0700

    Completed move rectangle function

--- ascii_art.py
+++ ascii_art.py
@@ -47,17 +47,23 @@ def get_fill_instructions(start_x, start_y, end_x, end_y):
 
     for y in range(start_y, end_y + 1):
         for x in range(start_x, end_x + 1):
-            fill_matrix[y][x] = True
+            # If the coordinates fall outside of matrix/boundary, 
+            # characters will not render. Rectangle is effectively cropped.
+            try:
+                fill_matrix[y][x] = True
+            except:
+                continue
 
     return fill_matrix
 
 
-def add_rect_to_dict(rectangles, fill_matrix, fill_char):
+def add_rect_to_dict(rectangles, fill_matrix, fill_char, 
+                     start_x, start_y, end_x, end_y):
     """Add rectangle to rectangles dict"""
 
     id = len(rectangles) + 1
 
-    rectangles[id] = [fill_char, fill_matrix]
+    rectangles[id] = [fill_char, fill_matrix, [start_x, start_y, end_x, end_y]]
 
     return rectangles
 
@@ -82,7 +88,8 @@ def add_shape(canvas, rectangles):
 
     start_x, start_y, end_x, end_y, fill_char = get_attributes()
     fill_matrix = get_fill_instructions(start_x, start_y, end_x, end_y)
-    rectangles = add_rect_to_dict(rectangles, fill_matrix, fill_char)
+    rectangles = add_rect_to_dict(rectangles, fill_matrix, fill_char, 
+                                  start_x, start_y, end_x, end_y)
     canvas = update_canvas(canvas, rectangles)
     print_canvas(canvas)
 
@@ -95,7 +102,7 @@ def print_canvas(canvas):
 
 
 def clear_canvas():
-    """Delete all shapes from canvas"""
+    """Returns canvas with no shapes"""
 
     canvas = [
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -109,9 +116,13 @@ def clear_canvas():
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]]
 
-    rectangles = {}
+    return canvas
 
-    return canvas, rectangles
+
+def clear_rectangles():
+    """Returns dictionary with no triangles"""
+
+    return {}
 
 
 def select_rectangle(rectangles):
@@ -135,25 +146,74 @@ def change_rect_fill(canvas, rectangles):
     rectangles[rectangle][0] = fill_char
     canvas = update_canvas(canvas, rectangles)
     print_canvas(canvas)
-    
 
-def move_rectangle():
+
+def select_axis():
+
+    options = """
+Along which axis would you like to move the rectangle?
+x-axis (moving left or right)
+y-axis (moving up or down)
+    """
+    print(options)
+
+    return input("Enter x or y: ")
+
+
+def update_rectangle(rectangles, rectangle, axis, num_spaces):
+    """Updates fill_matrix and starting/ending coords of a single rectangle"""
+
+    old_start_x, old_start_y, old_end_x, old_end_y = rectangles[rectangle][2]
+    
+    if axis == "x":
+        start_x = old_start_x + num_spaces
+        start_y = old_start_y
+        end_x = old_end_x + num_spaces
+        end_y = old_end_y
+
+    elif axis == "y":
+        start_x = old_start_x
+        start_y = old_start_y - num_spaces
+        end_x = old_end_x
+        end_y = old_end_y - num_spaces
+
+    fill_matrix = get_fill_instructions(start_x, start_y, end_x, end_y)
+
+    rectangles[rectangle][1] = fill_matrix
+    rectangles[rectangle][2] = [start_x, start_y, end_x, end_y]
+
+    return rectangles
+
+
+def move_rectangle(canvas, rectangles):
     """Move rectangle along x- or y-axis, in positive or negative direction"""
 
-    pass
+    rectangle = select_rectangle(rectangles)
+    axis = select_axis()
+    num_spaces = int(input(
+                 "Move how many spaces? (Use negative num for left or down) "))
+    rectangles = update_rectangle(rectangles, rectangle, axis, num_spaces)
+    canvas = update_canvas(canvas, rectangles)
+    print_canvas(canvas)
+
 
 def main():
-    canvas, rectangles = clear_canvas()
+    rectangles = clear_rectangles()
 
     while True:
+        canvas = clear_canvas()
+
         action = select_action()
 
         if action == "1":
             add_shape(canvas, rectangles)
+        elif action =="2":
+            move_rectangle(canvas, rectangles)
         elif action == "3":
             change_rect_fill(canvas, rectangles)
         elif action == "4":
-            canvas, rectangles = clear_canvas()
+            canvas = clear_canvas()
+            rectangles  = clear_rectangles()
             print_canvas(canvas)
 
 
